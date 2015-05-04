c18d2b4ddabd3ef831d91f714ee17f3df5bf31b9
Vanessa Barbosa <vanessabmdf@gmail.com>
2015-05-04 20:39:10 -0300

    Dando print de tudo ._.

--- src/RetroPropagação.java
+++ src/RetroPropagação.java
@@ -8,7 +8,7 @@ public class RetroPropagação {
 	static double[][] w1CamadaDeEntrada = new double [2][3];
 	static double[][] erroW1 = new double [2][3];
 	static double[] w2CamadaDeSaida = new double [3];
-	static double[] erroW2=new double[3];
+	static double[] erroW2=new double[2];
 	static double[][] entrada = new double[4][2];
 	static double[] saida = new double[4];
 	static double[] saidaCalculada = new double[4];
@@ -17,7 +17,7 @@ public class RetroPropagação {
 	static double H0Bias=1.0;
 	static double[] hidden = new double[2];
 	static double erroSaida=0;
-	static double[] erroIntermediario= new double[3];
+	static double[] erroIntermediario= new double[2];
 	
 	static final int numMaximoEpoca=15;
 	static final double alphaTaxaDeAprendizagem=0.5;
@@ -76,29 +76,40 @@ public class RetroPropagação {
 		System.out.println("Saida Calculada:"+saidaCalculada[index]+"\n");
 		
 		erroSaida = saida[index]*(1-saida[index])*(saidaCalculada[index]-saida[index]);
+		System.out.println("Erro Saída: "+erroSaida);
 		
-		erroIntermediario[0]=H0Bias*(1-H0Bias)*somatorio(erroSaida, w2CamadaDeSaida);
+		//erroIntermediario[0]=H0Bias*(1-H0Bias)*somatorio(erroSaida, w2CamadaDeSaida);
 		for (int linha = 0; linha < hidden.length; linha++) {
-			erroIntermediario[linha+1] = hidden[linha]*(1-hidden[linha])*(somatorio(erroSaida, w2CamadaDeSaida));
+			erroIntermediario[linha] = hidden[linha]*(1-hidden[linha])*(somatorio(erroSaida, w2CamadaDeSaida));
 		}
 		
-		erroW2[0]=alphaTaxaDeAprendizagem * erroSaida * H0Bias ;
-		w2CamadaDeSaida[0]= w2CamadaDeSaida[0]+erroW2[0];
+//		erroW2[0]=alphaTaxaDeAprendizagem * erroSaida * H0Bias ;
+//		w2CamadaDeSaida[0]= w2CamadaDeSaida[0]+erroW2[0];
+//		System.out.println("Novo w2 0"+" :"+w2CamadaDeSaida[0]);
 		
 		for (int linha = 0; linha < hidden.length; linha++) {
-			erroW2[linha+1]=alphaTaxaDeAprendizagem * erroSaida * hidden[linha];
-			w2CamadaDeSaida[linha+1]= w2CamadaDeSaida[linha+1]+erroW2[linha+1];
-		}
-		
-		erroW1[0][0]=alphaTaxaDeAprendizagem*erroIntermediario[0]*x0Bias;
-		erroW1[1][0]=alphaTaxaDeAprendizagem*erroIntermediario[0]*x0Bias;
-		
-		for (int linha = 0; linha < entrada[0].length; linha++) {
-			erroW1[0][linha+1]=alphaTaxaDeAprendizagem*erroIntermediario[linha+1]*xEntrada[linha];
-			w1CamadaDeEntrada[0][linha+1]= w1CamadaDeEntrada[0][linha+1]+erroW1[0][linha+1];
+			erroW2[linha]=alphaTaxaDeAprendizagem * erroSaida * hidden[linha];
+			System.out.println("Erro w2 "+linha+" :"+erroW2[linha]);
+			w2CamadaDeSaida[linha]= w2CamadaDeSaida[linha+1]+erroW2[linha];
 			
-			erroW1[1][linha+1]=alphaTaxaDeAprendizagem*erroIntermediario[linha+1]*xEntrada[linha];
-			w1CamadaDeEntrada[1][linha+1]= w1CamadaDeEntrada[1][linha+1]+erroW1[1][linha+1];
+//			System.out.println("Novo w2 "+(linha+1)+" :"  +w2CamadaDeSaida[linha+1]);
+		}
+		
+//		erroW1[0][0]=alphaTaxaDeAprendizagem*erroIntermediario[0]*x0Bias;
+//		erroW1[1][0]=alphaTaxaDeAprendizagem*erroIntermediario[0]*x0Bias;
+				
+		for (int linha = 0; linha < entrada[0].length; linha++) {
+			erroW1[0][linha]=alphaTaxaDeAprendizagem*erroIntermediario[linha]*xEntrada[linha];
+			w1CamadaDeEntrada[0][linha]= w1CamadaDeEntrada[0][linha]+erroW1[0][linha];
+			System.out.println("Erro w1 0 "+linha+" :"+w1CamadaDeEntrada[0][linha]);
+			
+//			System.out.println("Novo w1 0 "+(linha+1)+" :"+w1CamadaDeEntrada[0][linha+1]);
+			
+			erroW1[1][linha]=alphaTaxaDeAprendizagem*erroIntermediario[linha]*xEntrada[linha];
+			w1CamadaDeEntrada[1][linha]= w1CamadaDeEntrada[1][linha]+erroW1[1][linha];
+			System.out.println("Erro w1 1 "+linha+" :"+w1CamadaDeEntrada[1][linha]);
+			
+//			System.out.println("Novo w1 1 "+(linha+1)+" :"+w1CamadaDeEntrada[1][linha+1]);
 		}
 		
 	}
